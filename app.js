08d942d89499838a8bdee93e0ce7d177801932d6
PandaaxY <pandaaxytb@gmail.com>
2025-05-13 16:11:46 +0200

    All Card

--- app.js
+++ app.js
@@ -12,17 +12,33 @@ async function displayData() {
 
     data.results.forEach(user => {
         const userCard = document.createElement("user-card");
+        const randomAmount = Math.floor(Math.random() * 10000) + 1;
         userCard.innerHTML = `
-            <img src="${user.picture.large}" alt="User Picture">
-            <h3>${user.name.first} ${user.name.last}</h3>
-            <p>${user.email}</p>
-            <p>${user.phone}</p>
-            <p>${user.location.city}, ${user.location.country}</p>
+            <div class="user-card">
+                <h2>${randomAmount}€</h2>
+                <img src="${user.picture.medium}" alt="User Picture">
+                <h3>${user.name.first} ${user.name.last}</h3>
+                <p>${user.email}</p>
+                <p>${user.phone}</p>
+                <p>${user.location.city}, ${user.location.country}</p>
+                <p>${user.gender}</p>
+            </div>
         `;
         userContainer.appendChild(userCard);
     });
-
+    
 }
 
+ displayData()
 
-// displayData()
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
+toggle.addEventListener("click", () => {
+    if (window.innerWidth <= 600) {
+        sidebar.classList.toggle("open");
+        sidebar.classList.remove("close");
+    } else {
+        sidebar.classList.toggle("close");
+        sidebar.classList.remove("open");
+    }
+});
